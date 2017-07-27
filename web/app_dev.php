b8a374ede7423ebe4947fc173c2374a5d8d4c3e6
Elorfin <penin.axel@gmail.com>
2017-07-27 09:44:22 +0200

    cleans API entrypoints

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,10 +1,8 @@
 <?php
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read https://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
-// for more information
-//umask(0000);
+
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
@@ -12,15 +10,15 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || PHP_SAPI === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    exit(
+        sprintf('You are not allowed to access this file. Check %s for more information.', basename(__FILE__))
+    );
 }
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
 Debug::enable();
 $kernel = new AppKernel('dev', true);
-if (PHP_VERSION_ID < 70000) {
-    $kernel->loadClassCache();
-}
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
