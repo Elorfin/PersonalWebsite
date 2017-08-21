6bb9f0f86171edd06f5ef5657615a13de1dcca7f
Elorfin <penin.axel@gmail.com>
2017-08-21 11:03:19 +0200

    implements first UI version for competencies

--- src/AppBundle/Resources/js/app/sections/competencies/components/main.jsx
+++ src/AppBundle/Resources/js/app/sections/competencies/components/main.jsx
@@ -1,11 +1,196 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { PropTypes as T } from 'prop-types'
+import { connect } from 'react-redux'
 
-const Competencies = () =>
+// TODO : do not import the whole namespace
+import * as d3 from 'd3'
+
+import { select } from 'main/app/selectors'
+
+function flatten(root) {
+  let nodes = []
+
+  function recurse(node) {
+    if (node.children) node.children.forEach(recurse)
+    nodes.push(node)
+  }
+
+  recurse(root)
+
+  return nodes
+}
+
+class CompetenciesGraph extends Component {
+  constructor(props) {
+    super(props)
+
+    this.updateGraph = this.updateGraph.bind(this)
+  }
+
+  componentDidMount() {
+    this.updateGraph()
+
+    window.addEventListener('resize', this.updateGraph)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateGraph)
+  }
+
+  updateGraph() {
+    const graph = d3.select(this.graph)
+
+    graph.attr('width', this.container.offsetWidth)
+
+    const sizeModifier = d => 1 + (d.count().value / 10) + (1 / (d.depth + 1))
+
+    const size     = d => 30 * sizeModifier(d)
+    const fontSize = d => 8 * sizeModifier(d)
+
+    // get data to display
+    let nodes = []
+    let links = []
+    this.props.competencies.map(competency => {
+      const root = d3.hierarchy(competency)
+
+      nodes = nodes.concat(flatten(root))
+      links = links.concat(root.links())
+    })
+
+    // link data to SVG DOM
+    let node = graph.selectAll('.node').data(nodes)
+    let link = graph.selectAll('.link').data(links)
+
+    let linkEnter = link.enter()
+      .append('line')
+      .attr('class', 'link')
+      .attr('stroke', link => link.source.data.color[0])
+      .attr('stroke-width', link => Math.floor(4 * sizeModifier(link.source)))
+
+    link.exit().remove()
+
+    let nodeEnter = node.enter()
+      .append('g')
+      .attr('class', 'node')
+
+    nodeEnter
+      .append('circle')
+      .attr('r', size)
+      .attr('fill', d => d.data.color[0])
+
+    nodeEnter
+      .append('text')
+      .text(d => d.data.name)
+      .attr('font-family', 'Century Gothic, Arial')
+      .attr('font-size', fontSize)
+      .attr('fill', d => d.data.color[1] ? d.data.color[1] : 'black')
+      .attr('stroke', d => d.children ? (d.data.color[1] ? d.data.color[1] : 'black') : 'transparent')
+      .attr('textLength', d => size(d) * 2 - 30)
+      .attr('lengthAdjust', 'spacingAndGlyphs')
+      .attr('text-anchor', 'middle')
+      .attr('alignment-baseline', 'central')
+
+    nodeEnter
+      .call(d3.drag()
+        .on('start', d => {
+          if (!d3.event.active) {
+            simulation.alphaTarget(0.3).restart()
+          }
+
+          d.fx = d.x
+          d.fy = d.y
+        })
+        .on('drag', d => {
+          d.fx = d3.event.x
+          d.fy = d3.event.y
+        })
+        .on('end', d => {
+          if (!d3.event.active) {
+            simulation.alphaTarget(0)
+          }
+
+          d.fx = null
+          d.fy = null
+        })
+      )
+
+    // removes deleted
+    node.exit().remove()
+
+    let simulation = d3.forceSimulation(nodes)
+      .force('link', d3.forceLink(links).iterations(5))
+      .force('collide',d3.forceCollide(d => size(d) + 2).iterations(5))
+      .force('charge', d3.forceManyBody())
+      .force('center', d3.forceCenter(graph.attr('width') / 2, graph.attr('height') / 2))
+      .force('y', d3.forceY(0))
+      .force('x', d3.forceX(0))
+      .on('tick', () => {
+        nodeEnter
+          .attr('transform', d => {
+            // node position is locked inside the SVG bounding box
+            d.x = Math.max(size(d), Math.min(graph.attr('width') - size(d), d.x))
+            d.y = Math.max(size(d), Math.min(graph.attr('height') - size(d), d.y))
+
+            return `translate(${d.x}, ${d.y})`
+          })
+
+        linkEnter
+          .attr('x1', d => d.source.x)
+          .attr('y1', d => d.source.y)
+          .attr('x2', d => d.target.x)
+          .attr('y2', d => d.target.y)
+      })
+  }
+
+  render() {
+    return (
+      <div
+        ref={element => this.container = element}
+        className="graph-container"
+      >
+        <svg
+          ref={element => this.graph = element}
+          className="competencies-graph"
+          height={this.props.height}
+        />
+      </div>
+    )
+  }
+}
+
+CompetenciesGraph.propTypes = {
+  height: T.number,
+  competencies: T.arrayOf(T.shape({
+    id: T.number.isRequired,
+    name: T.string.isRequired
+  })).isRequired
+}
+
+CompetenciesGraph.defaultProps = {
+  height: 800
+}
+
+const Competencies = props =>
   <section className="container app-section">
     <h2 className="sr-only">Competencies</h2>
-    Competencies
+    <CompetenciesGraph
+      height={800}
+      competencies={props.competencies}
+    />
   </section>
 
+Competencies.propTypes = {
+  competencies: T.array.isRequired
+}
+
+const mapStateToProps = state => {
+  return {
+    competencies: select.competencies(state)
+  }
+}
+
+const ConnectedCompetencies = connect(mapStateToProps, {})(Competencies)
+
 export {
-  Competencies
+  ConnectedCompetencies as Competencies
 }
--- src/AppBundle/Resources/js/app/store.js
+++ src/AppBundle/Resources/js/app/store.js
@@ -34,11 +34,203 @@ const initialState = {
   competencies: [
     {
       id: 1,
-      name: 'PHP',
-      description: '',
-      mastery: 5,
-      versions: ['4', '5', '7'], // for techno
-      related: []
+      name: 'Back-End development',
+      color: ['#3c5c7f', '#b4d4f7'],
+      children: [
+        { id: 10, name: 'Java', color: ['#6484a7'] },
+        {
+          id: 11,
+          name: 'PHP',
+          color: ['#6484a7'],
+          children: [
+            { id: 111, name: 'symfony 2', color: ['#8caccf'] },
+            { id: 112, name: 'Silex', color: ['#8caccf'] },
+            { id: 113, name: 'Doctrine 2', color: ['#8caccf'] },
+            { id: 114, name: 'PHPUnit', color: ['#8caccf'] },
+            { id: 115, name: 'composer', color: ['#8caccf'] },
+            {
+              id: 116,
+              name: 'PSR',
+              color: ['#8caccf'],
+              children: [
+                { id: 1161, name: 'PSR-0', color: ['#b4d4f7'] },
+                { id: 1161, name: 'PSR-1', color: ['#b4d4f7'] },
+                { id: 1161, name: 'PSR-2', color: ['#b4d4f7'] },
+                { id: 1161, name: 'PSR-3', color: ['#b4d4f7'] },
+                { id: 1161, name: 'PSR-4', color: ['#b4d4f7'] }
+              ]
+            }
+          ]
+        },
+        {
+          id: 12,
+          name: 'NodeJS',
+          color: ['#6484a7'],
+          children: [
+            { id: 121, name: 'NPM', color: ['#8caccf'] },
+            { id: 121, name: 'yarn', color: ['#8caccf'] },
+            { id: 121, name: 'Karma', color: ['#8caccf'] },
+          ]
+        },
+        {
+          id: 13,
+          name: 'Database',
+          color: ['#6484a7'],
+          children: [
+            {
+              id: 131,
+              name: 'SQL',
+              color: ['#8caccf'],
+              children: [
+                { id: 1311, name: 'MySQL', color: ['#b4d4f7'] },
+                { id: 1312, name: 'PostgreSQL', color: ['#b4d4f7'] }
+              ]
+            },
+            { id: 132, name: 'MongoDB', color: ['#8caccf'] }
+          ]
+        }
+      ]
+    },
+
+    {
+      id: 2,
+      name: 'Front-End development',
+      color: ['#3c7f5c', '#b4f7d4'],
+      children: [
+        {
+          id: 21,
+          name: 'JavaScript',
+          color: ['#64a784'],
+          children: [
+            { id: 211, name: 'React', color: ['#8ccfac'] },
+            { id: 212, name: 'Redux', color: ['#8ccfac'] },
+            { id: 213, name: 'Angular 1', color: ['#8ccfac'] },
+            { id: 214, name: 'D3', color: ['#8ccfac'] },
+            { id: 215, name: 'ThreeJS', color: ['#8ccfac'] }
+          ]
+        },
+        {
+          id: 22,
+          name: 'HTML',
+          color: ['#64a784'],
+          children: [
+            { id: 221, name: 'HTML 5', color: ['#8ccfac'] },
+            { id: 222, name: 'Twig', color: ['#8ccfac'] }
+          ]
+        },
+        {
+          id: 23,
+          name: 'CSS',
+          color: ['#64a784'],
+          children: [
+            { id: 231, name: 'LESS', color: ['#8ccfac'] },
+            { id: 232, name: 'Sass', color: ['#8ccfac'] },
+            { id: 233, name: 'Bootstrap', color: ['#8ccfac'] }
+          ]
+        },
+        { id: 24, name: 'SVG', color: ['#64a784'] },
+        { id: 25, name: 'WCAG', color: ['#64a784'] }
+      ]
+    },
+
+    {
+      id: 3,
+      name: 'Middleware',
+      color: ['#5c3c7f', '#d4b4f7'],
+      children: [
+        { id: 31, name: 'RabbitMQ', color: ['#8464a7'] },
+        { id: 32, name: 'Talend Open Studio', color: ['#8464a7'] }
+      ]
+    },
+
+    {
+      id: 4,
+      name: 'Software engineering',
+      color: ['#7f3c5c', '#f7b4d4'],
+      children: [
+        { id: 41, name: 'Unit Testing', color: ['#a76484'] },
+        { id: 41, name: 'External API integration', color: ['#a76484'] },
+        {
+          id: 42,
+          name: 'Architecture',
+          color: ['#a76484'],
+          children: [
+            { id: 421, name: 'MVC', color: ['#cf8cac'] },
+            { id: 422, name: 'MVVM', color: ['#cf8cac'] },
+            { id: 423, name: 'REST', color: ['#cf8cac'] }
+          ]
+        },
+        {
+          id: 43,
+          name: 'Methodology',
+          color: ['#a76484'],
+          children: [
+            { id: 431, name: 'SCRUM', color: ['#cf8cac'] },
+            { id: 432, name: 'Test Driven Development', color: ['#cf8cac'] }
+          ]
+        },
+        {
+          id: 44,
+          name: 'Continuous Integration',
+          color: ['#a76484'],
+          children: [
+            { id: 441, name: 'Travis CI', color: ['#cf8cac'] },
+            { id: 442, name: 'Scrutinizer', color: ['#cf8cac'] }
+          ]
+        },
+        {
+          id: 44,
+          name: 'Modelization',
+          color: ['#a76484'],
+          children: [
+            { id: 441, name: 'UML', color: ['#cf8cac'] },
+            { id: 442, name: 'Merise', color: ['#cf8cac'] }
+          ]
+        }
+      ]
+    },
+
+    {
+      id: 5,
+      name: 'System & Network',
+      color: ['#7f5c3c', '#f7d4b4'],
+      children: [
+        { id: 51, name: 'Apache', color: ['#a78464'] },
+        { id: 52, name: 'Nginx', color: ['#a78464'] },
+        {
+          id: 53,
+          name: 'OS',
+          color: ['#a78464'],
+          children: [
+            { id: 531, name: 'Windows', color: ['#cfac8c'] },
+            { id: 532, name: 'Linux', color: ['#cfac8c'] },
+            { id: 533, name: 'Mac', color: ['#cfac8c'] }
+          ]
+        }
+      ]
+    },
+
+    {
+      id: 6,
+      name: 'Software',
+      color: ['#F5A700', '#FFECC2'],
+      children: [
+        { id: 61, name: 'Adobe Photoshop', color: ['#FFC547'] },
+        { id: 62, name: 'The Gimp', color: ['#FFC547'] },
+        { id: 63, name: 'Adobe Illustrator', color: ['#FFC547'] },
+        { id: 63, name: 'Blender', color: ['#FFC547'] },
+        { id: 63, name: 'UDK', color: ['#FFC547'] },
+        {
+          id: 64,
+          name: 'IDE',
+          color: ['#FFC547'],
+          children: [
+            { id: 641, name: 'PHP Storm', color: ['#FFD885'] },
+            { id: 642, name: 'Eclipse', color: ['#FFD885'] },
+            { id: 643, name: 'Netbeans', color: ['#FFD885'] }
+          ]
+        }
+      ]
     }
   ],
   experiences: [
